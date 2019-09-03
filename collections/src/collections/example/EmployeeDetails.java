1a34d301e43c7a45a8684c02a47c93835b653078
b4ibmjava32@iiht.tech <b4ibmjava32@iiht.tech>
2019-09-03 08:40:49 +0530

    collection

---
+++ collections/src/collections/example/EmployeeDetails.java
@@ -0,0 +1,24 @@
+package collections.example;
+
+public class EmployeeDetails {
+	public int id;
+	public String fname;
+	public String lname;
+	public double cgpa;
+	
+
+	public EmployeeDetails() {
+		// TODO Auto-generated constructor stub
+	super();
+	}
+	
+     public EmployeeDetails(int id,String fname,String lname,double cgpa) {
+    	 super();
+    	 this.id=id;
+    	 this.cgpa=cgpa;
+    	 this.fname=fname;
+    	 this.lname=lname;
+    	 
+     }
+     
+}
---
+++ collections19_08/src/collections/example/SetExample.java
@@ -0,0 +1,11 @@
+package collections.example;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+
+public class SetExample {
+
+}
